2b1db212d34e9c2871c2f0bebb31f1d0ed32c13b
Nate Mueller <inathan.m@gmail.com>
2023-10-13 17:13:24 -0500

    active collection

--- contracts/interfaces/IMadSBT.sol
+++ contracts/interfaces/IMadSBT.sol
@@ -5,52 +5,48 @@ pragma solidity >=0.8.0;
  * @notice IMadSBT interface
  */
 interface IMadSBT {
-  struct CollectionData {
-    uint256 startingTokenId; // first token id in this collection
-    uint256 totalSupply; // total tokens minted for a given id
-    uint256 availableSupply; // total tokens available for a given id
-    uint256 totalRedeemed; // total tokens redeemed
-    uint256 creatorId; // lens profile id, also the IDA index
-    address creatorAddress; // lens profile address
-    string uri; // metadata uri
-  }
+    struct CollectionData {
+        uint256 startingTokenId; // first token id in this collection
+        uint256 totalSupply; // total tokens minted for a given id
+        uint256 availableSupply; // total tokens available for a given id
+        uint256 totalRedeemed; // total tokens redeemed
+        uint256 creatorId; // lens profile id, also the IDA index
+        address creatorAddress; // lens profile address
+        string uri; // metadata uri
+    }
 
-  enum WormholePayloadAction {
-    Mint,
-    Burn
-  }
+    enum WormholePayloadAction {
+        Mint,
+        Burn
+    }
 
-  event CreateCollection(address creator, uint256 profileId, uint256 collectionId, uint256 availableSupply);
-  event UpdateRewardUnits(uint256 collectionId, address subscriber, uint128 newUnits);
+    event CreateCollection(address creator, uint256 profileId, uint256 collectionId, uint256 availableSupply);
+    event UpdateRewardUnits(uint256 collectionId, address subscriber, uint128 newUnits);
 
-  function createCollection(address, uint256, uint256, string memory) external returns (uint256);
+    function createCollection(address, uint256, uint256, string memory) external returns (uint256);
 
-  function mint(address, uint256) external returns (uint256);
-  function burn(uint256) external;
-  function handleRewardsUpdate(address, uint256, uint8) external;
-  function redeemInterimRewardUnits(uint256) external;
-  function burnOnSubscriptionCanceled(uint256, address) external;
+    function mint(address, uint256) external returns (uint256);
+    function burn(uint256) external;
+    function handleRewardsUpdate(address, uint256, uint8) external;
+    function redeemInterimRewardUnits(uint256) external;
+    function burnOnSubscriptionCanceled(uint256, address) external;
 
-  function creatorProfileId(uint256) external view returns (uint256);
-  function contractURI() external view returns (string memory);
-  function hasMinted(address, uint256) external view returns (bool);
-  function rewardUnitsOf(address, uint256) external view returns (uint128);
-  function getLevel(uint256) external view returns (uint256);
-  function getTokenLevel(uint256) external view returns (uint256);
-  function totalMinted() external view returns (uint256);
-  function subscriptionHandler() external view returns (address);
+    function creatorProfileId(uint256) external view returns (uint256);
+    function contractURI() external view returns (string memory);
+    function hasMinted(address, uint256) external view returns (bool);
+    function rewardUnitsOf(address, uint256) external view returns (uint128);
+    function getLevel(uint256) external view returns (uint256);
+    function getTokenLevel(uint256) external view returns (uint256);
+    function totalMinted() external view returns (uint256);
+    function subscriptionHandler() external view returns (address);
 
-  // direct mapping to struct CollectionData
-  function collectionData(uint256) external view returns (
-    uint256,
-    uint256,
-    uint256,
-    uint256,
-    uint256,
-    address,
-    string memory
-  );
+    // direct mapping to struct CollectionData
+    function collectionData(uint256)
+        external
+        view
+        returns (uint256, uint256, uint256, uint256, uint256, address, string memory);
 
-  function tokenToCollection(uint256) external view returns (uint256);
-  function actionToRewardUnits(uint8) external view returns (uint128);
+    function tokenToCollection(uint256) external view returns (uint256);
+    function actionToRewardUnits(uint8) external view returns (uint128);
+    function activeCollection(address) external view returns (uint256);
 }
