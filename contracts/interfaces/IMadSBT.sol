0f95a55dc462807ed67c87d25ca7874ce2dce9a9
Carlos Beltran <imthatcarlos@gmail.com>
2023-11-15 08:52:08 -0500

    update interface

--- contracts/interfaces/IMadSBT.sol
+++ contracts/interfaces/IMadSBT.sol
@@ -1,18 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 
+interface IERC1155 {
+  function balanceOf(address, uint256) external view returns (uint256);
+}
+
 /**
  * @notice IMadSBT interface
+ * @dev It directly implements ERC721, and supports IERC1155#blaanceOf
  */
 interface IMadSBT {
+  struct WrappedCollectionData {
+    address contractAddress;
+    ContractType contractType;
+    uint256 pointedCollectionId;
+    uint256 linkedCollectionId; // if the collection creator makes a regular collection, link them for subgraph
+  }
+
   struct CollectionData {
     uint256 startingTokenId; // first token id in this collection
     uint256 totalSupply; // total tokens minted for a given id
     uint256 availableSupply; // total tokens available for a given id
     uint256 totalRedeemed; // total tokens redeemed
     uint256 creatorId; // lens profile id, also the IDA index
+    uint128 totalInterimRewardUnits; // total reward units distributed before the reward token set
     address creatorAddress; // lens profile address
     string uri; // metadata uri
+    bool isWrapped; // its a pointer to another collection
   }
 
   enum WormholePayloadAction {
@@ -20,8 +34,15 @@ interface IMadSBT {
     Burn
   }
 
+  enum ContractType {
+    ERC_721,
+    ERC_1155
+  }
+
   event CreateCollection(address creator, uint256 profileId, uint256 collectionId, uint256 availableSupply);
+  event CreateWrappedCollection(address creator, uint256 profileId, uint256 collectionId);
   event UpdateRewardUnits(uint256 collectionId, address subscriber, uint128 newUnits);
+  event LinkWrappedCollection(address creator, uint256 profileId, uint256 collectionId, uint256 wrappedCollectionId);
 
   function createCollection(address, uint256, uint256, string memory) external returns (uint256);
   function mint(address, uint256) external returns (uint256);
@@ -32,27 +53,16 @@ interface IMadSBT {
   function batchRewardsUpdate(address[] calldata, uint256, uint8) external;
   function distributeRewards(uint256, uint256) external;
 
-  function creatorProfileId(uint256) external view returns (uint256);
   function contractURI() external view returns (string memory);
   function hasMinted(address, uint256) external view returns (bool);
   function rewardUnitsOf(address, uint256) external view returns (uint128);
+  function totalRewardUnits(uint256) external view returns (uint128);
   function getLevel(uint256) external view returns (uint256);
   function getTokenLevel(uint256) external view returns (uint256);
   function totalMinted() external view returns (uint256);
   function subscriptionHandler() external view returns (address);
 
-  // direct mapping to struct CollectionData
-  function collectionData(uint256) external view returns (
-    uint256,
-    uint256,
-    uint256,
-    uint256,
-    uint256,
-    address,
-    string memory
-  );
-
-  function activeCollection(address) external view returns (uint256);
+  function activeCollection(uint256) external view returns (uint256);
   function tokenToCollection(uint256) external view returns (uint256);
   function actionToRewardUnits(uint8) external view returns (uint128);
 }
