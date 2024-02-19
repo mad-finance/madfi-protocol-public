c4e6495dc00ccbe1886acf97bce136676510f068
Nate Mueller <inathan.m@gmail.com>
2024-02-19 14:04:15 -0500

    add exact input

--- contracts/interfaces/IUniswap.sol
+++ contracts/interfaces/IUniswap.sol
@@ -65,4 +65,17 @@ interface ISwapRouter {
         uint256 amountIn,
         uint160 sqrtPriceLimitX96
     ) external returns (uint256 amountOut);
+
+    struct ExactInputParams {
+        bytes path;
+        address recipient;
+        uint256 deadline;
+        uint256 amountIn;
+        uint256 amountOutMinimum;
+    }
+
+    /// @notice Swaps `amountIn` of one token for as much as possible of another along the specified path
+    /// @param params The parameters necessary for the multi-hop swap, encoded as `ExactInputParams` in calldata
+    /// @return amountOut The amount of the received token
+    function exactInput(ExactInputParams calldata params) external payable returns (uint256 amountOut);
 }
